47841696303d5e169ac19581d534ed4f235bc556
Zubaer Rahman <zubaer9898@gmail.com>
2022-04-17 23:03:39 +0100

    Enabled storage config for application run

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -1,17 +1,23 @@
 package com.example.grpc.client.grpcclient;
 
+import com.example.grpc.client.grpcclient.config.FileStorageProperties;
+import com.example.grpc.client.grpcclient.service.StorageService;
 import com.example.grpc.server.grpcserver.HelloRequest;
 import com.example.grpc.server.grpcserver.HelloResponse;
 import com.example.grpc.server.grpcserver.HelloServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class GrpcClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -46,6 +52,14 @@ public class GrpcClientApplication extends SpringBootServletInitializer {
 
 		channel.shutdown();*/
 //		SpringApplication.run(GrpcClientApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
