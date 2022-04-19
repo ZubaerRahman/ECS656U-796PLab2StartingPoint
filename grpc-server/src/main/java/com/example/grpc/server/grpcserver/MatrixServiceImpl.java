d2e920a6a86229898652cd852fa526f5099cc1cb
Zubaer Rahman <zubaer9898@gmail.com>
2022-04-19 18:28:55 +0100

    attempt to create server

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -6,28 +6,84 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase
 {
+	private static final int MAX = 4;
+
+	public static int[][] matrixToArray(MatrixBlocks matrix) {
+		int[][] temp = new int[MAX][MAX];
+		temp[0][0] = matrix.getC00();
+		temp[0][1] = matrix.getC01();
+		temp[1][0] = matrix.getC10();
+		temp[1][1] = matrix.getC11();
+		return temp;
+	}
+
+	public MatrixBlocks arrayToMatrix(int[][] array) {
+		MatrixBlocks C = MatrixBlocks.newBuilder()
+				.setC00(array[0][0])
+				.setC01(array[0][1])
+				.setC10(array[1][0])
+				.setC11(array[1][1])
+				.build();
+
+		return C;
+	}
+
+	private static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		return;
+	}
+
+	public static int[][] performMatrixBlockMultiplication(int A[][], int B[][]) {
+		int C[][] = new int[MAX][MAX];
+		C[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+		C[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+		C[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+		C[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+		return C;
+	}
+
+	public static int[][] performMatrixBlockAddition(int A[][], int B[][]) {
+		int C[][] = new int[MAX][MAX];
+		for (int i = 0; i < C.length; i++) {
+			for (int j = 0; j < C.length; j++) {
+				C[i][j] = A[i][j] + B[i][j];
+			}
+		}
+		return C;
+	}
+
 	@Override
-	public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-	{
+	public void addBlock(MatrixRequest request, StreamObserver<MatrixResponse> reply) {
 		System.out.println("Request received from client:\n" + request);
-		int C00=request.getA00()+request.getB00();
-    		int C01=request.getA01()+request.getB01();
-		int C10=request.getA10()+request.getB10();
-		int C11=request.getA11()+request.getB11();
-		MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
+		int[][] matrixA = matrixToArray(request.getA());
+		int[][] matrixB = matrixToArray(request.getB());
+		int[][] newMatrix = performMatrixBlockAddition(matrixA, matrixB);
+		MatrixResponse response = MatrixResponse.newBuilder()
+				.setC(arrayToMatrix(newMatrix))
+				.build();
 		reply.onNext(response);
 		reply.onCompleted();
 	}
+
 	@Override
-    	public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-    	{
-        	System.out.println("Request received from client:\n" + request);
-        	int C00=request.getA00()*request.getB00()+request.getA01()*request.getB10();
-		int C01=request.getA00()*request.getB01()+request.getA01()*request.getB11();
-		int C10=request.getA10()*request.getB00()+request.getA11()*request.getB10();
-		int C11=request.getA10()*request.getB01()+request.getA11()*request.getB11();
-        MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
-        reply.onNext(response);
-        reply.onCompleted();
-    }
+	public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixResponse> reply) {
+		System.out.println("Request received from client:\n" + request);
+		int[][] matrixA = matrixToArray(request.getA());
+		printMatrix(matrixA);
+		int[][] matrixB = matrixToArray(request.getB());
+		printMatrix(matrixB);
+		int[][] newMatrix = performMatrixBlockMultiplication(matrixA, matrixB);
+		MatrixResponse response = MatrixResponse.newBuilder()
+				.setC(arrayToMatrix(newMatrix))
+				.build();
+		System.out.println("Response matrix is: \n" + response);
+		reply.onNext(response);
+		reply.onCompleted();
+	}
+
 }
